c58e0a96842061a098809284c63bbdd933ccd1d1
DavePearce <david.pearce@ecs.vuw.ac.nz>
2021-02-16 17:25:04 +1300

    Working on constraint implementation

--- src/jsmt/core/Constraint.java
+++ src/jsmt/core/Constraint.java
@@ -78,18 +78,71 @@ public abstract class Constraint {
         }
     }
 
-    private static final class Polynomial {
+    public static final class Polynomial {
         private final Term[] terms;
 
         public Polynomial(int coefficient,int variable) {
             this.terms = new Term[]{new Term(coefficient, variable)};
         }
 
-        private static class Term {
+        public Polynomial(Term... terms) {
+            this.terms = terms;
+        }
+
+        public Polynomial add(Polynomial p) {
+            // NOTE: could be more efficient!
+            for(int i=0;i!=terms.length;++i) {
+                p = p.add(terms[i]);
+            }
+            return p;
+        }
+
+        private Polynomial add(Term t) {
+            for (int i = 0; i != terms.length; ++i) {
+                Term ith = terms[i];
+                if (Arrays.equals(ith.variables, t.variables)) {
+                    // No need to append!
+                    Term[] nterms = Arrays.copyOf(terms, terms.length);
+                    nterms[i] = new Term(ith.coefficient + t.coefficient, ith.variables);
+                    //
+                    return new Polynomial(nterms);
+                }
+            }
+            //
+            Term[] nterms = Arrays.copyOf(terms, terms.length + 1);
+            nterms[terms.length] = t;
+            Arrays.sort(nterms);
+            return new Polynomial(nterms);
+        }
+
+        public String toString() {
+            String r = "";
+            for(int i=0;i!=terms.length;++i) {
+                if(i != 0) {
+                    r = r + " + ";
+                }
+                r += terms[i].toString();
+            }
+            return r;
+        }
+
+        public String toString(String[] vars) {
+            String r = "";
+            for(int i=0;i!=terms.length;++i) {
+                if(i != 0) {
+                    r = r + " + ";
+                }
+                r += terms[i].toString(vars);
+            }
+            return r;
+        }
+
+        private static class Term implements Comparable<Term> {
             private final int coefficient;
             private final int[] variables;
 
             public Term(int coefficient, int... variables) {
+                Arrays.sort(variables);
                 this.coefficient = coefficient;
                 this.variables = variables;
             }
@@ -101,6 +154,39 @@ public abstract class Constraint {
                 }
                 return v;
             }
+
+            @Override
+            public int compareTo(Term o) {
+                int c = Arrays.compare(variables,o.variables);
+                return (c != 0) ? c : Integer.compare(coefficient, o.coefficient);
+            }
+
+            public boolean equals(Object o) {
+                if(o instanceof Term) {
+                    Term t = (Term) o;
+                    return coefficient == t.coefficient && Arrays.equals(variables,t.variables);
+                }
+                return false;
+            }
+
+            public int hashCode() {
+                return coefficient ^ Arrays.hashCode(variables);
+            }
+
+            public String toString() {
+                String r = Integer.toString(coefficient);
+                for(int i=0;i!=variables.length;++i) {
+                    r += "*" + variables[i];
+                }
+                return "("+r+")";
+            }
+            public String toString(String[] vars) {
+                String r = Integer.toString(coefficient);
+                for(int i=0;i!=variables.length;++i) {
+                    r += "*" + vars[variables[i]];
+                }
+                return "("+r+")";
+            }
         }
     }
 
@@ -111,5 +197,7 @@ public abstract class Constraint {
         Constraint.Set set = new Constraint.Set(2,ieq);
         //
         System.out.println("SAT: " + Arrays.toString(set.sat()));
+        //
+        System.out.println("POLY: " + new Polynomial(new Polynomial.Term(1, 0), new Polynomial.Term(2, 1)).toString(new String[]{"x","y"}));
     }
 }
